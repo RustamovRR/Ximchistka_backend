1ec7817fb2bb76c4ed7d73416b5b3384745cd644
rustamovriskiddin123@gmail.com <rustamovriskiddin123@gmail.com>
2020-10-28 10:15:58 +0500

    edited route login

--- routes/login.js
+++ routes/login.js
@@ -2,13 +2,12 @@ const express = require('express');
 const router = express.Router();
 const Admin = require('../models/Admin');
 
-
 router.post('/', async (req, res) => {
 	try {
-		const admin = await Admin.find({ login: req.body.login });
+		const admin = await Admin.findById('5f98f6612e07524d23e9323e')
 
-		if(admin) {
-			if(admin.password === req.body.password) {
+		if (admin) {
+			if (admin.password === req.body.password) {
 				return res.status(200).json({
 					success: true,
 					message: "Tasdiqlandi!"
@@ -23,7 +22,7 @@ router.post('/', async (req, res) => {
 	}
 	catch (err) {
 		res.status(400).json({
-			success: true,
+			success: false,
 			message: err.message
 		});
 	}
