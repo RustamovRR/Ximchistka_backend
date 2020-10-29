94340be1785adbf9f913cb108bf7fefe32bfff69
rustamovriskiddin123@gmail.com <rustamovriskiddin123@gmail.com>
2020-10-29 15:24:24 +0500

    editied minor

--- routes/login.js
+++ routes/login.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const Admin = require('../models/Admin');
+const Admin = require('../models/Admin')
+require('dotenv/config')
+
+const user_Id = process.env.user_Id
 
 router.post('/', async (req, res) => {
 	try {
-		const admin = await Admin.findById('5f98f6612e07524d23e9323e')
+		const admin = await Admin.findById(user_Id)
 
 		if (admin) {
 			if (admin.password === req.body.password) {
