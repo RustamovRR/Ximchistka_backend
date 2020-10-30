5dd6e29e96e3db192e7fcb470e63e21854347edb
rustamovriskiddin123@gmail.com <rustamovriskiddin123@gmail.com>
2020-10-30 11:43:00 +0500

    second update

--- Backend Node.js/routes/login.js
+++ Backend Node.js/routes/login.js
@@ -1,18 +1,20 @@
 const express = require('express');
 const router = express.Router();
-const Admin = require('../models/Admin');
+const Admin = require('../models/Admin')
+require('dotenv/config')
 
+const user_Id = process.env.user_Id
 
 router.post('/', async (req, res) => {
 	try {
-		const admin = await Admin.find({ login: req.body.login });
+		const admin = await Admin.findById(user_Id)
 
-		if(admin) {
-			if(admin.password === req.body.password) {
-				return res.status(200).json({
+		if (admin) {
+			if (admin.password === req.body.password) {
+				return res.json({
 					success: true,
 					message: "Tasdiqlandi!"
-				});
+				})
 			}
 		}
 
@@ -23,7 +25,7 @@ router.post('/', async (req, res) => {
 	}
 	catch (err) {
 		res.status(400).json({
-			success: true,
+			success: false,
 			message: err.message
 		});
 	}
